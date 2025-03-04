88d4d9b67c6df0d53081cedbce43267020d832ea
Alan Dana Omar <lan-92@hotmail.com>
2025-03-04 17:04:19 +0100

    init

---
+++ models/models.ts
@@ -0,0 +1,52 @@
+export interface ArtistCredit extends ArtistMinimal {
+  //if name is null take from artist.name
+  //id from artist.id
+  joinphrase: string | null;
+}
+
+export interface Release {
+  id: string;
+  title: string;
+  artistId: string;
+  date: string | null; //if null take from release-group.date
+  "release-group": { "primary-type": string | null };
+  "artist-credit": ArtistCredit[];
+  media: Media[];
+  cover_image: string | null;
+}
+
+export interface Media {
+  "track-count": number;
+  tracks: Track[] | null;
+}
+
+export interface Track {
+  id: string;
+  title: string;
+  "artist-credit": ArtistCredit[];
+  length: number | null;
+  lyrics: string | null;
+}
+
+export interface ArtistMinimal {
+  id: string;
+  name: string;
+  image: string | null;
+  profile: string | null;
+}
+
+export interface Artist extends ArtistMinimal {
+  aliases: { name: string }[];
+  releases: Release[];
+  relations: { url: { resource: string } }[];
+}
+
+export interface ArtistSearchResult {
+  artists: Artist[];
+  count: number;
+}
+
+export interface ReleaseResult {
+  releases: Release[];
+  "release-count": number;
+}
---
+++ utils/dateUtil.ts
@@ -0,0 +1,56 @@
+import { Release } from "../models/models";
+
+export const sortReleasesByDate = (releases: Release[]) => {
+  releases.sort((a, b) => {
+      const dateA = parseDateToNumber(a.date);
+      const dateB = parseDateToNumber(b.date);
+      return dateB - dateA;
+  });
+};
+
+const dateTimeMin = -8640000000000000;
+
+const parseDateToNumber = (dateStr: string | null): number => {
+    if (!dateStr?.trim()) return dateTimeMin;
+  
+    const parsedDate = new Date(dateStr);
+    if (!isNaN(parsedDate.getTime())) {
+      return parsedDate.getTime();
+    }
+  
+    const dateParts = dateStr.split('-');
+  
+    if (dateParts.length === 1) dateParts.push('01');
+  
+    if (dateParts.length === 2) dateParts.push('01');
+  
+    if (dateParts.length !== 3) return dateTimeMin;
+  
+    const day = dateParts[2].padStart(2, '0');
+    const month = dateParts[1].padStart(2, '0');
+  
+    const validDate = new Date(`${dateParts[0]}-${month}-${day}`);
+  
+    if (isNaN(validDate.getTime())) {
+      console.error("Invalid date:", dateStr);
+      return dateTimeMin;
+    }
+  
+    return validDate.getTime();
+};
+
+export const formatDate = (dateStr: string | null): string => {
+  const unixTimeStamp = parseDateToNumber(dateStr);
+  if (unixTimeStamp === dateTimeMin) return "Unknown date";
+  return new Date(unixTimeStamp).toLocaleDateString("nb-NO", {
+    year: "numeric",
+      month: "long",
+      day: "numeric"
+  });
+};
+
+export const isFutureDate = (dateString: string | null): boolean => {
+  const now = new Date();
+  now.setHours(0, 0, 0, 0);
+  return parseDateToNumber(dateString) > now.getTime();
+};

5ecb2e80b8fa8269a2094b33d2227b5a3661ccab
Alan Dana Omar <lan-92@hotmail.com>
2025-03-10 03:59:34 +0100

    padstart on day

--- utils/dateUtil.ts
+++ utils/dateUtil.ts
@@ -44,8 +44,8 @@ export const formatDate = (dateStr: string | null): string => {
   if (unixTimeStamp === dateTimeMin) return "Unknown date";
 
   const date = new Date(unixTimeStamp);
-  const day = date.getDate();
-  const month = String(date.getMonth() + 1).padStart(2, "0");;
+  const day = String(date.getDate()).padStart(2, "0")
+  const month = String(date.getMonth() + 1).padStart(2, "0");
   const year = date.getFullYear();
 
   return `${day}.${month}.${year}`;

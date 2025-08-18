5fe853d87d005829cd56ae63a8c03cce28dbf467
Alan Dana Omar <lan-92@hotmail.com>
2025-08-19 00:27:10 +0200

    added getReleaseTypes to helpers

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,1 +1,21 @@
 export const nameWithDisambiguation = (disambiguation: string | null, name: string) => disambiguation ? name + ' (' + disambiguation + ')' : name;
+
+export const getReleaseTypes = (): string[] => {
+    return [
+        'Album',
+        'Single',
+        'EP',
+        'Appearance',
+        'Other',
+        'Compilation',
+        'Soundtrack',
+        'Broadcast',
+        'Spokenword',
+        'Interview',
+        'Audiobook',
+        'Live',
+        'Remix',
+        'DJ-Mix',
+        'Mixtape/Street'
+    ];
+}
